cc63c85fbf1048efe980c573d22c694f2ad1c1f4
DorianI <>
2021-05-12 14:08:32 +0000

    style changes in js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let age = 0
-var day = 'day'
+var day = 'date'
 document.getElementById('button').addEventListener('click', alarm)
 
 function alarm () {
   age = document.getElementById('age').value
   age = parseInt(age)
-  if ((age < 18) && (day != 'sunday' || day != 'saturday')) {
+  if ((age < 18) && (day !== 'sunday' || day !== 'saturday')) {
     document.getElementById('answer').innerHTML = 'Time for school!'
   } else {
     document.getElementById('answer').innerHTML = 'Time to relax for the weekend!'
