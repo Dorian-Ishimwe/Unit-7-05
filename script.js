22c331b7cfb359b31a7a60534b4fc23f4afec018
DorianI <>
2021-05-12 14:03:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let age = 0
+var day = 'day'
+document.getElementById('button').addEventListener('click', alarm)
+
+function alarm () {
+  age = document.getElementById('age').value
+  age = parseInt(age)
+  if ((age < 18) && (day != 'sunday' || day != 'saturday')) {
+    document.getElementById('answer').innerHTML = 'Time for school!'
+  } else {
+    document.getElementById('answer').innerHTML = 'Time to relax for the weekend!'
+  }
+}
